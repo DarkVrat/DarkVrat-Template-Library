da910388814f4b29e1b7e40548eac51eed44b044
DarkVrat <118006878+DarkVrat@users.noreply.github.com>
2024-01-13 18:50:00 +0500

    implemented iterators for vectors, as well as forward and remove reference template

--- DVTL/headers/utility.h
+++ DVTL/headers/utility.h
@@ -3,9 +3,27 @@
 
 namespace DVTL 
 {
-	template<typename T> inline T&& move(T& obj) noexcept { return static_cast<T&&>(obj); }
+	template<typename T> struct remove_reference			{ typedef T type; };
+	template<typename T> struct remove_reference<T&>		{ typedef T type; };
+	template<typename T> struct remove_reference<T&&>		{ typedef T type; };
+
+	template<typename T> using remove_reference_t = typename remove_reference<T>::type;
+
+	template<typename T> constexpr bool is_lvalue_reference_v		= false;
+	template<typename T> constexpr bool is_lvalue_reference_v<T&>	= true;
+
+	template<typename T> constexpr remove_reference_t<T>&& move(T&& obj) noexcept { return static_cast<remove_reference_t<T>&&>(obj); }
+	template<typename T> constexpr T&& forward(remove_reference_t<T>& obj) noexcept { return static_cast<T&&>(obj); }
+	template<typename T> constexpr T&& forward(remove_reference_t<T>&& obj) noexcept 
+	{
+		static_assert(!is_lvalue_reference_v<T>, "bad forward call");
+		return static_cast<T&&>(obj);
+	}
 
 	template<typename T> inline void swap(T& left, T& right) { left.swap(right); }
+
+	template<typename T> inline T& max(T& left, T& right) { return left > right ? left, right; }
+	template<typename T> inline T& min(T& left, T& right) { return left < right ? left, right; }
 }
 
 #ifndef __PLACEMENT_NEW_INLINE
--- DVTL/headers/vector.h
+++ DVTL/headers/vector.h
@@ -10,6 +10,12 @@ namespace DVTL
 	class vector 
 	{
 	public:
+		//declaring iterators
+		class iterator;
+		class const_iterator;
+		class reverse_iterator;
+		class const_reverse_iterator;
+
 		//constructors, operators= and destructor
 					vector() noexcept;
 		explicit	vector(size_t size);
@@ -54,37 +60,229 @@ namespace DVTL
 		void push_back(const T& value);
 		void push_back(T&&);
 		void pop_back();
-		//template <class... Types> iterator emplace(const const_iterator& position, Types&&... args);
-		//template <class... Types> iterator emplace_back(Types&&... args);
-		//iterator inset(const const_iterator& position, const T& value);
-		//iterator inset(const const_iterator& position, T&& value);
-		//iterator inset(const const_iterator& position, size_t count, const T& value);
-		//iterator erase(const const_iterator& position);
-		//iterator erase(const const_iterator& first, const const_iterator& last);
+		template <class... Types> iterator emplace(const const_iterator& position, Types&&... args);
+		template <class... Types> iterator emplace_back(Types&&... args);
+		iterator inset(const const_iterator& position, const T& value);
+		iterator inset(const const_iterator& position, T&& value);
+		iterator inset(const const_iterator& position, size_t count, const T& value);
+		iterator erase(const const_iterator& position);
+		iterator erase(const const_iterator& first, const const_iterator& last);
 
-		//getting iterators
-		//iterator begin();
-		//iterator end();
-		//const_iterator begin()	const;
-		//const_iterator end()		const;
-		//const_iterator cbegin()	const;
-		//const_iterator cend()		const;
-		//reverse_iterator rbegin();
-		//reverse_iterator rend();
-		//const_reverse_iterator rbegin()	const;
-		//const_reverse_iterator rend()		const;
-		//const_reverse_iterator crbegin()	const;
-		//const_reverse_iterator crend()	const;
+		//getting iterators begin and end
+		inline iterator begin()	{ return iterator(m_data); }
+		inline iterator end()	{ return iterator(m_data + m_size); }
+		inline const_iterator begin()	const { return const_iterator(m_data); }
+		inline const_iterator end()		const { return const_iterator(m_data + m_size); }
+		inline const_iterator cbegin()	const { return const_iterator(m_data); }
+		inline const_iterator cend()	const { return const_iterator(m_data + m_size); }
+		inline reverse_iterator rbegin()	{ return reverse_iterator(m_data + m_size - 1); }
+		inline reverse_iterator rend()		{ return reverse_iterator(m_data - 1); }
+		inline const_reverse_iterator rbegin()	const { return const_reverse_iterator(m_data + m_size - 1); }
+		inline const_reverse_iterator rend()	const { return const_reverse_iterator(m_data - 1); }
+		inline const_reverse_iterator crbegin()	const { return const_reverse_iterator(m_data + m_size - 1); }
+		inline const_reverse_iterator crend()	const { return const_reverse_iterator(m_data - 1); }
 	private:
 		size_t m_capacity;
 		size_t m_size;
 		T* m_data;
 
+		//helper functions
 		void AllocateAndMove(size_t new_size);
 		void DestroyAndDealloc();
+		void ShiftLeft(size_t count);
+		void ShiftRight(size_t count);
+		void CheckValidIter(const_iterator& position);
 		size_t CalculateNewCapacity();
 	};
 
+	//defining an iterator interface
+	template<typename T>
+	class vector<T>::iterator 
+	{
+	public:
+		//constructors, operators= and destructor
+		iterator(T* ptr)								:ptr(ptr) {}
+		iterator(const vector<T>::reverse_iterator& it) :ptr(++it.ptr) {}
+		iterator(const iterator& right)					:ptr(right.ptr) {}
+		iterator(iterator&& right)						:ptr(right.ptr)	{ right.ptr = nullptr; }
+		iterator& operator=(const iterator& right)	{ ptr = right.ptr; return *this; }
+		iterator& operator=(iterator right)			{ ptr = right.ptr; right.ptr = nullptr; return *this; }
+		~iterator() {}
+
+		//logical operators
+		bool operator==(const iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const iterator& other) const { return ptr != other.ptr; }
+		bool operator<=(const iterator& other) const { return ptr <= other.ptr; }
+		bool operator>=(const iterator& other) const { return ptr >= other.ptr; }
+		bool operator< (const iterator& other) const { return ptr <  other.ptr; }
+		bool operator> (const iterator& other) const { return ptr >  other.ptr; }
+
+		//data access
+		T& operator*() const { return *ptr; }
+		T* operator->() const { return ptr; }
+		T& operator[](size_t index) const { return ptr[index]; }
+
+		//mathematical operators
+		iterator& operator++()			{ ++ptr; return *this; }
+		iterator& operator--()			{ --ptr; return *this; }
+		const iterator operator++(int)	{ iterator temp = *this; ++ptr; return temp; }
+		const iterator operator--(int)	{ iterator temp = *this; --ptr; return temp; }
+		iterator& operator+=(int n)		{ ptr += n; return *this; }
+		iterator& operator-=(int n)		{ ptr -= n; return *this; }
+		iterator operator+(int n) const	{ return iterator(ptr + n); }
+		iterator operator-(int n) const { return iterator(ptr - n); }
+		auto operator-(const iterator& other) const -> decltype(auto) {
+			return ptr - other ptr;
+		}
+
+		//transformation
+		vector<T>::const_iterator get_const() const		{ return vector<T>::const_iterator(*this); }
+		vector<T>::reverse_iterator get_revers() const	{ return vector<T>::reverse_iterator(*this); }
+
+	private:
+		T* ptr;
+	};
+
+	template<typename T>
+	class vector<T>::const_iterator
+	{
+	public:
+		//constructors, operators= and destructor
+		const_iterator(const T* ptr)								:ptr(ptr) {}
+		const_iterator(const vector<T>::iterator& it)				:ptr(it.ptr) {}
+		const_iterator(const vector<T>::const_reverse_iterator& it) :ptr(++it.ptr) {}
+		const_iterator(const const_iterator& right)					:ptr(right.ptr) {}
+		const_iterator(const_iterator&& right)						:ptr(right.ptr) { right.ptr = nullptr; }
+		const_iterator& operator=(const const_iterator& right)	{ ptr = right.ptr; return *this; }
+		const_iterator& operator=(const_iterator right)			{ ptr = right.ptr; right.ptr = nullptr; return *this; }
+		~const_iterator() {}
+
+		//logical operators
+		bool operator==(const const_iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const const_iterator& other) const { return ptr != other.ptr; }
+		bool operator<=(const const_iterator& other) const { return ptr <= other.ptr; }
+		bool operator>=(const const_iterator& other) const { return ptr >= other.ptr; }
+		bool operator< (const const_iterator& other) const { return ptr <  other.ptr; }
+		bool operator> (const const_iterator& other) const { return ptr >  other.ptr; }
+
+		//data access
+		const T& operator*() const { return *ptr; }
+		const T* operator->() const { return ptr; }
+		const T& operator[](size_t index) const { return ptr[index]; }
+
+		//mathematical operators
+		const_iterator& operator++()			{ ++ptr; return *this; }
+		const_iterator& operator--()			{ --ptr; return *this; }
+		const const_iterator operator++(int)	{ const_iterator temp = *this; ++ptr; return temp; }
+		const const_iterator operator--(int)	{ const_iterator temp = *this; --ptr; return temp; }
+		const_iterator& operator+=(int n)		{ ptr += n; return *this; }
+		const_iterator& operator-=(int n)		{ ptr -= n; return *this; }
+		const_iterator operator+(int n) const	{ return const_iterator(ptr + n); }
+		const_iterator operator-(int n) const	{ return const_iterator(ptr - n); }
+		auto operator-(const const_iterator& other) const -> decltype(auto) {
+			return ptr - other ptr;
+		}
+
+		//transformation
+		vector<T>::const_reverse_iterator get_straight() const { return vector<T>::const_reverse_iterator(*this); }
+
+	private:
+		const T* ptr;
+	};
+
+	template<typename T>
+	class vector<T>::reverse_iterator
+	{
+	public:
+		//constructors, operators= and destructor
+		reverse_iterator(T* ptr)						:ptr(ptr) {}
+		reverse_iterator(const vector<T>::iterator& it) :ptr(--it.ptr) {}
+		reverse_iterator(const reverse_iterator& right) :ptr(right.ptr) {}
+		reverse_iterator(reverse_iterator&& right)		:ptr(right.ptr) { right.ptr = nullptr; }
+		reverse_iterator& operator=(const reverse_iterator& right)	{ ptr = right.ptr; return *this; }
+		reverse_iterator& operator=(reverse_iterator right)			{ ptr = right.ptr; right.ptr = nullptr; return *this; }
+		~reverse_iterator() {}
+
+		//logical operators
+		bool operator==(const reverse_iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const reverse_iterator& other) const { return ptr != other.ptr; }
+		bool operator<=(const reverse_iterator& other) const { return ptr >= other.ptr; }
+		bool operator>=(const reverse_iterator& other) const { return ptr <= other.ptr; }
+		bool operator< (const reverse_iterator& other) const { return ptr >  other.ptr; }
+		bool operator> (const reverse_iterator& other) const { return ptr <  other.ptr; }
+
+		//data access
+		T& operator*() const { return *ptr; }
+		T* operator->() const { return ptr; }
+
+		//mathematical operators
+		reverse_iterator& operator++()			{ --ptr; return *this; }
+		reverse_iterator& operator--()			{ ++ptr; return *this; }
+		const reverse_iterator operator++(int)	{ reverse_iterator temp = *this; --ptr; return temp; }
+		const reverse_iterator operator--(int)	{ reverse_iterator temp = *this; ++ptr; return temp; }
+		reverse_iterator& operator+=(int n)		{ ptr -= n; return *this; }
+		reverse_iterator& operator-=(int n)		{ ptr += n; return *this; }
+		reverse_iterator operator+(int n) const { return reverse_iterator(ptr - n); }
+		reverse_iterator operator-(int n) const { return reverse_iterator(ptr + n); }
+		auto operator-(const reverse_iterator& other) const -> decltype(auto) {
+			return ptr - other ptr;
+		}
+
+		//transformation
+		vector<T>::const_reverse_iterator get_const() const { return vector<T>::const_reverse_iterator(&this); }
+		vector<T>::iterator get_straight()	const			{ return vector<T>::iterator(*this); }
+
+	private:
+		T* ptr;
+	};
+
+	template<typename T>
+	class vector<T>::const_reverse_iterator
+	{
+	public:
+		//constructors, operators= and destructor
+		const_reverse_iterator(const T* ptr)							:ptr(ptr) {}
+		const_reverse_iterator(const vector<T>::reverse_iterator& it)	:ptr(it.ptr) {}
+		const_reverse_iterator(const vector<T>::const_iterator& it)		:ptr(--it.ptr) {}
+		const_reverse_iterator(const const_reverse_iterator& right)		:ptr(right.ptr) {}
+		const_reverse_iterator(const_reverse_iterator&& right)			:ptr(right.ptr) { right.ptr = nullptr; }
+		const_reverse_iterator& operator=(const const_reverse_iterator& right)	{ ptr = right.ptr; return *this; }
+		const_reverse_iterator& operator=(const_reverse_iterator right)			{ ptr = right.ptr; right.ptr = nullptr; return *this; }
+		~const_reverse_iterator() {}
+
+		//logical operators
+		bool operator==(const const_reverse_iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const const_reverse_iterator& other) const { return ptr != other.ptr; }
+		bool operator<=(const const_reverse_iterator& other) const { return ptr >= other.ptr; }
+		bool operator>=(const const_reverse_iterator& other) const { return ptr <= other.ptr; }
+		bool operator< (const const_reverse_iterator& other) const { return ptr >  other.ptr; }
+		bool operator> (const const_reverse_iterator& other) const { return ptr <  other.ptr; }
+
+		//data access
+		const T& operator*() const { return *ptr; }
+		const T* operator->() const { return ptr; }
+
+		//mathematical operators
+		const_reverse_iterator& operator++()			{ --ptr; return *this; }
+		const_reverse_iterator& operator--()			{ ++ptr; return *this; }
+		const const_reverse_iterator operator++(int)	{ const_reverse_iterator temp = *this; --ptr; return temp; }
+		const const_reverse_iterator operator--(int)	{ const_reverse_iterator temp = *this; ++ptr; return temp; }
+		const_reverse_iterator& operator+=(int n)		{ ptr -= n; return *this; }
+		const_reverse_iterator& operator-=(int n)		{ ptr += n; return *this; }
+		const_reverse_iterator operator+(int n) const	{ return const_reverse_iterator(ptr - n); }
+		const_reverse_iterator operator-(int n) const	{ return const_reverse_iterator(ptr + n); }
+		auto operator-(const const_reverse_iterator& other) const -> decltype(auto) {
+			return ptr - other ptr;
+		}
+
+		//transformation
+		vector<T>::const_iterator get_straight() const { return vector<T>::const_iterator(*this); }
+
+	private:
+		const T* ptr;
+	};
+
+	//constructors, operators= and destructor
 	template<typename T>
 	inline vector<T>::vector() noexcept : m_capacity(0), m_size(0), m_data(nullptr){}
 
@@ -157,7 +355,7 @@ namespace DVTL
 		DestroyAndDealloc();
 	}
 
-
+	//references to vector elements
 	template<typename T>
 	inline T& vector<T>::at(size_t index)
 	{
@@ -178,6 +376,7 @@ namespace DVTL
 	template<typename T> inline const T& vector<T>::back()	const	{ return at(m_size - 1); }
 	template<typename T> inline const T& vector<T>::operator[](size_t index) const noexcept { return m_data[index]; }
 
+	//working with data
 	template<typename T>
 	inline void vector<T>::reserve(size_t count)
 	{
@@ -243,6 +442,7 @@ namespace DVTL
 		right.m_capacity = temp_capacity;
 	}
 
+	//adding and removing elements to a vector
 	template<typename T>
 	inline void vector<T>::push_back(const T& value)
 	{
@@ -269,6 +469,69 @@ namespace DVTL
 	}
 
 	template<typename T>
+	template<class ...Types>
+	inline vector<T>::iterator vector<T>::emplace(const const_iterator& position, Types && ...args)
+	{
+		CheckValidIter(position);
+
+		return iterator();
+	}
+
+	template<typename T>
+	template<class ...Types>
+	inline vector<T>::iterator vector<T>::emplace_back(Types && ...args)
+	{
+		if (m_size == m_capacity)
+			AllocateAndMove(CalculateNewCapacity());
+
+		allocator::construct(m_data, m_size++, forward<Types>(args)... );
+
+		return iterator(m_data+m_size-1);
+	}
+
+	template<typename T>
+	inline vector<T>::iterator vector<T>::inset(const const_iterator& position, const T& value)
+	{
+		CheckValidIter(position);
+
+		return iterator();
+	}
+
+	template<typename T>
+	inline vector<T>::iterator vector<T>::inset(const const_iterator& position, T&& value)
+	{
+		CheckValidIter(position);
+
+		return iterator();
+	}
+
+	template<typename T>
+	inline vector<T>::iterator vector<T>::inset(const const_iterator& position, size_t count, const T& value)
+	{
+		CheckValidIter(position);
+
+		return iterator();
+	}
+
+	template<typename T>
+	inline vector<T>::iterator vector<T>::erase(const const_iterator& position)
+	{
+		CheckValidIter(position);
+
+		return iterator();
+	}
+
+	template<typename T>
+	inline vector<T>::iterator vector<T>::erase(const const_iterator& first, const const_iterator& last)
+	{
+		CheckValidIter(first);
+		CheckValidIter(last);
+
+		return iterator();
+	}
+
+	//helper functions
+	template<typename T>
 	inline void vector<T>::AllocateAndMove(size_t new_size)
 	{
 		T* newData = allocator::allocate<T>(new_size);
@@ -294,14 +557,30 @@ namespace DVTL
 	}
 
 	template<typename T>
+	inline void vector<T>::ShiftLeft(size_t count)
+	{
+
+	}
+
+	template<typename T>
+	inline void vector<T>::ShiftRight(size_t count)
+	{
+		if (m_size + count > m_capacity)
+			AllocateAndMove(m_size + count);
+		
+	}
+
+	template<typename T>
+	inline void vector<T>::CheckValidIter(const_iterator& position)
+	{
+		if (position < cbegin() || position > cend()) throw "vector emplace iterator outside range";
+	}
+
+	template<typename T>
 	inline size_t vector<T>::CalculateNewCapacity()
 	{
 		return m_capacity + m_capacity/2 + 1;
 	}
 }
-
-
-
-
 
 #endif // !DVTL_VECTOR_H
